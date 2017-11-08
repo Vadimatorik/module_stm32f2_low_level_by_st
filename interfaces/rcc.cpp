565d9f20be3a6f6ac249ae4202776936dfa7f8ea
Vadim Deryabkin <deryabkinda@mail.ru>
2017-11-08 12:51:52 +0700

    Написана оболочка rcc.

--- interfaces/rcc.cpp
+++ interfaces/rcc.cpp
@@ -1,6 +1,15 @@
 #include "rcc.h"
 
 RCC_RESULT rcc::set_cfg ( uint32_t number_cfg_set ) const {
-    (void)number_cfg_set;
+    if ( number_cfg_set >= this->number_cfg ) return RCC_RESULT::ERROR_CFG_NUMBER;
+
+    HAL_RCC_DeInit();
+
+    if ( HAL_RCC_ClockConfig( ( RCC_ClkInitTypeDef* )&this->array_cfg_st[ number_cfg_set ].clk_cfg, this->array_cfg_st[ number_cfg_set ].f_latency ) != HAL_OK )
+        return  RCC_RESULT::ERROR_CLK_INIT;
+
+    if ( HAL_RCC_OscConfig( ( RCC_OscInitTypeDef* )&this->array_cfg_st[ number_cfg_set ].osc_cfg ) != HAL_OK )
+        return RCC_RESULT::ERROR_OSC_INIT;
+
     return RCC_RESULT::OK;
 }
--- interfaces/rcc.h
+++ interfaces/rcc.h
@@ -1,14 +1,20 @@
 #include "mc_hardware_interfaces_rcc.h"
+#include "stm32f2xx_hal_rcc.h"
+#include "stm32f2xx_hal_flash.h"
+#include <stdint.h>
 
 struct rcc_cfg {
-
+    RCC_OscInitTypeDef          osc_cfg;
+    RCC_ClkInitTypeDef          clk_cfg;
+    uint32_t                    f_latency;
 };
 
 class rcc : public rcc_base {
 public:
-    rcc( const rcc_cfg* const array_cfg_st ) : array_cfg_st( array_cfg_st ) {}
+    rcc( const rcc_cfg* const array_cfg_st, const uint32_t number_cfg ) : array_cfg_st( array_cfg_st ), number_cfg( number_cfg ) {}
     RCC_RESULT set_cfg ( uint32_t number_cfg_set ) const;
 
 private:
-    const rcc_cfg* const array_cfg_st;
+    const rcc_cfg*              const array_cfg_st;
+    const uint32_t              number_cfg;
 };
