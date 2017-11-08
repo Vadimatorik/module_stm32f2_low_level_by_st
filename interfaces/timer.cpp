7425a1ecd99774d02b4afce3fd74fb4ad2af69a0
Vadim Deryabkin <deryabkinda@mail.ru>
2017-11-08 12:03:00 +0700

    Заглушки для timer.

---
+++ interfaces/timer.cpp
@@ -0,0 +1,29 @@
+#include "timer.h"
+
+void tim_comp_one_channel::reinit ( void ) const {
+
+}
+
+void tim_comp_one_channel::on ( void ) const {
+
+}
+
+void tim_comp_one_channel::off ( void ) const {
+
+}
+
+void tim_interrupt::reinit ( void ) const {
+
+}
+
+void tim_interrupt::on ( void ) const {
+
+}
+
+void tim_interrupt::off ( void ) const {
+
+}
+
+void tim_interrupt::clear_interrupt_flag ( void ) const {
+
+}
--- interfaces/timer.h
+++ interfaces/timer.h
@@ -2,17 +2,34 @@
 
 #include "mc_hardware_interfaces_timer.h"
 
+struct tim_comp_one_channel_cfg {
+
+};
+
 class tim_comp_one_channel : public tim_comp_one_channel_base {
-    tim_comp_one_channel();
+public:
+    tim_comp_one_channel( const tim_comp_one_channel_cfg* const cfg ) : cfg( cfg ) {}
     void reinit ( void ) const;
     void on ( void ) const;
     void off ( void ) const;
+
+private:
+    const tim_comp_one_channel_cfg* const cfg;
+};
+
+struct tim_interrupt_cfg {
+
 };
 
 class tim_interrupt : public tim_interrupt_base {
 public:
+    tim_interrupt( const tim_interrupt_cfg* const cfg ) : cfg( cfg ) {}
+
     void reinit ( void ) const;
     void on ( void ) const;
     void off ( void ) const;
     void clear_interrupt_flag ( void ) const;
+
+private:
+    const tim_interrupt_cfg* const cfg;
 };
