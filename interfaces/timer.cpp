6608cdd8a5cd2ba881cbcbfd7b9d7e962ee6aed1
Vadimatorik <Deryabkinda@mail.ru>
2018-01-07 12:54:47 +0700

    Дописал таймеры (все 3).

--- interfaces/timer.cpp
+++ interfaces/timer.cpp
@@ -1,5 +1,65 @@
 #include "timer.h"
 
+// Включаем тактирование SPI.
+static void init_clk_tim ( TIM_TypeDef* tim ) {
+    switch ( ( uint32_t )tim ) {
+#ifdef TIM
+		case	TIM_BASE:		__HAL_RCC_TIM_CLK_ENABLE();			break;
+#endif
+#ifdef TIM1
+		case    TIM1_BASE:		__HAL_RCC_TIM1_CLK_ENABLE();		break;
+#endif
+#ifdef TIM2
+		case    TIM2_BASE:		__HAL_RCC_TIM2_CLK_ENABLE();		break;
+#endif
+#ifdef TIM3
+		case    TIM3_BASE:		__HAL_RCC_TIM3_CLK_ENABLE();		break;
+#endif
+#ifdef TIM4
+		case    TIM4_BASE:		__HAL_RCC_TIM4_CLK_ENABLE();		break;
+#endif
+#ifdef TIM5
+		case    TIM5_BASE:		__HAL_RCC_TIM5_CLK_ENABLE();		break;
+#endif
+#ifdef TIM6
+		case    TIM6_BASE:		__HAL_RCC_TIM6_CLK_ENABLE();		break;
+#endif
+#ifdef TIM7
+		case    TIM7_BASE:		__HAL_RCC_TIM7_CLK_ENABLE();		break;
+#endif
+#ifdef TIM8
+		case    TIM8_BASE:		__HAL_RCC_TIM8_CLK_ENABLE();		break;
+#endif
+#ifdef TIM9
+		case    TIM9_BASE:		__HAL_RCC_TIM9_CLK_ENABLE();		break;
+#endif
+#ifdef TIM10
+		case    TIM10_BASE:		__HAL_RCC_TIM10_CLK_ENABLE();		break;
+#endif
+#ifdef TIM11
+		case    TIM11_BASE:		__HAL_RCC_TIM11_CLK_ENABLE();		break;
+#endif
+#ifdef TIM12
+		case    TIM12_BASE:		__HAL_RCC_TIM12_CLK_ENABLE();		break;
+#endif
+#ifdef TIM13
+		case    TIM13_BASE:		__HAL_RCC_TIM13_CLK_ENABLE();		break;
+#endif
+#ifdef TIM14
+		case    TIM14_BASE:		__HAL_RCC_TIM14_CLK_ENABLE();		break;
+#endif
+#ifdef TIM15
+		case    TIM15_BASE:		__HAL_RCC_TIM15_CLK_ENABLE();		break;
+#endif
+#ifdef TIM16
+		case    TIM16_BASE:		__HAL_RCC_TIM16_CLK_ENABLE();		break;
+#endif
+    };
+}
+
+//**********************************************************************
+// tim_comp_one_channel
+//**********************************************************************
 tim_comp_one_channel::tim_comp_one_channel ( const tim_comp_one_channel_cfg* const cfg ) : cfg( cfg ) {
 	this->hal_tim_cfg.Instance						= this->cfg->tim;
 
@@ -12,10 +72,11 @@ tim_comp_one_channel::tim_comp_one_channel ( const tim_comp_one_channel_cfg* con
 
 	this->hal_tim_ch_cfg.OCMode						= TIM_OCMODE_TOGGLE;
 	this->hal_tim_ch_cfg.OCPolarity					= this->cfg->polarity;
-	this->hal_tim_ch_cfg.Pulse						= this->cfg->pulse;
+	this->hal_tim_ch_cfg.Pulse						= 0;
 }
 
 void tim_comp_one_channel::reinit ( void ) const {
+	init_clk_tim( this->cfg->tim );
 	HAL_TIM_OC_DeInit( &this->hal_tim_cfg );
 	HAL_TIM_OC_Init( &this->hal_tim_cfg );
 	HAL_TIM_OC_ConfigChannel( &this->hal_tim_cfg, &this->hal_tim_ch_cfg, this->cfg->out_channel );
@@ -29,6 +90,44 @@ void tim_comp_one_channel::off ( void ) const {
 	HAL_TIM_OC_Stop( &this->hal_tim_cfg, this->cfg->out_channel );
 }
 
+//**********************************************************************
+// tim_pwm_one_channel
+//**********************************************************************
+tim_pwm_one_channel::tim_pwm_one_channel ( const tim_pwm_one_channel_cfg* const cfg ) : cfg( cfg ) {
+	this->hal_tim_cfg.Instance						= this->cfg->tim;
+
+	this->hal_tim_cfg.Channel						= this->cfg->tim_channel;
+	this->hal_tim_cfg.Init.AutoReloadPreload		= TIM_AUTORELOAD_PRELOAD_ENABLE;
+	this->hal_tim_cfg.Init.ClockDivision			= TIM_CLOCKDIVISION_DIV1;
+	this->hal_tim_cfg.Init.CounterMode				= TIM_COUNTERMODE_UP;
+	this->hal_tim_cfg.Init.Period					= this->cfg->period;
+	this->hal_tim_cfg.Init.Prescaler				= this->cfg->prescaler;
+
+	this->hal_tim_ch_cfg.OCMode						= TIM_OCMODE_PWM2;
+	this->hal_tim_ch_cfg.OCPolarity					= this->cfg->polarity;
+}
+
+void tim_pwm_one_channel::reinit ( void ) const {
+	init_clk_tim( this->cfg->tim );
+	HAL_TIM_PWM_DeInit( &this->hal_tim_cfg );
+	HAL_TIM_PWM_Init( &this->hal_tim_cfg );
+	HAL_TIM_PWM_ConfigChannel( &this->hal_tim_cfg, &this->hal_tim_ch_cfg, this->cfg->out_channel );
+}
+
+void tim_pwm_one_channel::on ( void ) const {
+	HAL_TIM_PWM_Start( &this->hal_tim_cfg, this->cfg->out_channel );
+}
+
+void tim_pwm_one_channel::off ( void ) const {
+	HAL_TIM_PWM_Stop( &this->hal_tim_cfg, this->cfg->out_channel );
+}
+
+void tim_pwm_one_channel::duty_set ( float duty ) const {
+	__HAL_TIM_SET_COMPARE( &this->hal_tim_cfg, this->cfg->out_channel, this->cfg->period * duty );
+}
+//**********************************************************************
+// tim_interrupt
+//**********************************************************************
 tim_interrupt::tim_interrupt( const tim_interrupt_cfg* const cfg ) : cfg( cfg ) {
 	this->hal_tim_cfg.Instance						= this->cfg->tim;
 
--- interfaces/timer.h
+++ interfaces/timer.h
@@ -2,7 +2,11 @@
 
 #include "mc_hardware_interfaces_timer.h"
 #include "stm32f2xx_hal_tim.h"
+#include "stm32f2xx_hal_rcc.h"
 
+//**********************************************************************
+// tim_comp_one_channel
+//**********************************************************************
 struct tim_comp_one_channel_cfg {
 	// Используемый таймер.
 	TIM_TypeDef*				tim;
@@ -32,6 +36,40 @@ private:
     mutable	TIM_OC_InitTypeDef			hal_tim_ch_cfg;
 };
 
+//**********************************************************************
+// tim_pwm_one_channel
+//**********************************************************************
+struct tim_pwm_one_channel_cfg {
+	// Используемый таймер.
+	TIM_TypeDef*				tim;
+
+	// Параметры таймера.
+	HAL_TIM_ActiveChannel		tim_channel;
+	uint16_t					period;					// 0..0xFFFF.
+	uint16_t					prescaler;				// 0..0xFFFF.
+
+	// Параметры выходного канала.
+	uint8_t						out_channel;			// TIM_CHANNEL_x.
+	uint32_t					polarity;				// TIM_OCPOLARITY_LOW  / TIM_OCPOLARITY_HIGH.
+};
+
+class tim_pwm_one_channel : public tim_pwm_one_channel_base {
+public:
+	tim_pwm_one_channel( const tim_pwm_one_channel_cfg* const cfg );
+    void reinit ( void ) 			const;
+    void on ( void ) 				const;
+    void off ( void ) 				const;
+    void duty_set ( float duty ) 	const;
+
+private:
+    const tim_pwm_one_channel_cfg*		const cfg;
+    mutable TIM_HandleTypeDef			hal_tim_cfg;
+    mutable	TIM_OC_InitTypeDef			hal_tim_ch_cfg;
+};
+
+//**********************************************************************
+// tim_interrupt
+//**********************************************************************
 struct tim_interrupt_cfg {
 	TIM_TypeDef*				tim;
 
