b2bb4ac1ee4f729a3fb0e322ad2692f7a073f05e
Vadimatorik <Deryabkinda@mail.ru>
2018-01-07 13:07:40 +0700

    Доработал ADC на 1 канал.

--- interfaces/adc.cpp
+++ interfaces/adc.cpp
@@ -5,12 +5,11 @@ adc_one_channel::adc_one_channel( const adc_one_channel_cfg* const cfg ) {
     this->adc_st.Init.ClockPrescaler          = cfg->clock_prescaler;
     this->adc_st.Init.Resolution              = cfg->resolution;
     this->adc_st.Init.DataAlign               = cfg->data_align;
-    this->adc_st.Init.ScanConvMode            = DISABLE;
+    this->adc_st.Init.ScanConvMode            = ENABLE;
     this->adc_st.Init.EOCSelection            = ADC_EOC_SINGLE_CONV;
-    this->adc_st.Init.ContinuousConvMode      = DISABLE;
+    this->adc_st.Init.ContinuousConvMode      = ENABLE;
     this->adc_st.Init.NbrOfConversion         = 1;
     this->adc_st.Init.ExternalTrigConv        = ADC_SOFTWARE_START;
-    this->adc_st.Init.DMAContinuousRequests   = DISABLE;
 
     this->channel_cfg.Channel               = cfg->channel;
     this->channel_cfg.Rank                  = 1;
@@ -18,6 +17,7 @@ adc_one_channel::adc_one_channel( const adc_one_channel_cfg* const cfg ) {
 }
 
 void adc_one_channel::reinit ( void ) const {
+	this->on();
     HAL_ADC_DeInit( &this->adc_st );
     HAL_ADC_Init( &this->adc_st );
     HAL_ADC_ConfigChannel( &this->adc_st, &this->channel_cfg );
@@ -41,6 +41,10 @@ void adc_one_channel::off ( void ) const {
 
 void adc_one_channel::start_continuous_conversion ( void ) const {
     HAL_ADC_Start( &this->adc_st );
+}
+
+void adc_one_channel::stop_continuous_conversion ( void ) const {
+    HAL_ADC_Stop( &this->adc_st );
 }
 
 SPI::FUNC_RESULT adc_one_channel::get_measurement ( uint32_t& channel_measurement ) const {
--- interfaces/adc.h
+++ interfaces/adc.h
@@ -15,11 +15,12 @@ class adc_one_channel : public adc_one_channel_base {
 public:
     adc_one_channel( const adc_one_channel_cfg* const cfg );
 
-    void reinit ( void ) const;
-    void on ( void ) const;
-    void off ( void ) const;
-    void start_continuous_conversion ( void ) const;
-    SPI::FUNC_RESULT get_measurement ( uint32_t& channel_measurement ) const;
+    void reinit ( void )													const;
+    void on ( void )														const;
+    void off ( void )														const;
+    void start_continuous_conversion ( void )								const;
+    void stop_continuous_conversion ( void )								const;
+    SPI::FUNC_RESULT get_measurement ( uint32_t& channel_measurement )		const;
 
 private:
     mutable ADC_HandleTypeDef               adc_st;
