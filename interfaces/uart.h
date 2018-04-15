261f419ed1da542fe1690ec05833ba42bc191e85
Vadimatorik <deryabkinda@mail.ru>
2018-04-15 17:17:32 +0700

    UART в CPP стиле.

--- interfaces/uart.h
+++ interfaces/uart.h
@@ -7,34 +7,38 @@
 #include "stm32f2xx_hal_conf.h"
 #include "user_os.h"
 
-struct uart_cfg {
-	USART_TypeDef               *UARTx;
+struct uartCfg {
+	USART_TypeDef               *uart;
 	uint32_t					baudrate;
-	uint32_t					mode;							// UART_MODE_RX/UART_MODE_TX/UART_MODE_TX_RX.
-    DMA_Stream_TypeDef*         dma_tx;                         // Из мерии DMAx_Streamx.
-    uint32_t                    dma_tx_ch;                      // Из серии DMA_CHANNEL_x.
-	uint8_t						handler_prio;					// 1, 2..15.
+	uint32_t					mode;						// UART_MODE_RX/UART_MODE_TX/UART_MODE_TX_RX.
+    DMA_Stream_TypeDef*         dmaTx;						// Из мерии DMAx_Streamx.
+    uint32_t                    dmaTxCh;					// Из серии DMA_CHANNEL_x.
 };
 
-class uart : public uart_base {
+class Uart : public UartBase {
 public:
-	uart( const uart_cfg* const cfg );
-	bool 			reinit		( void ) const;
+	Uart( const uartCfg* const cfg );
 
-	void 			on     		( void ) const;
-	void 			off    		( void ) const;
+	bool 			reinit		( void )								const;
 
-    BASE_RESULT		tx			( const uint8_t* const  p_array_tx, const uint16_t& length, const uint32_t& timeout_ms ) const;
-    BASE_RESULT		get_byte	( uint8_t* data ) const;
+	void 			on     		( void )								const;
+	void 			off    		( void )								const;
 
-    void			irq_handler				( void ) const;
+    BASE_RESULT		tx			(	const uint8_t* const  p_array_tx,
+    								const uint16_t& length,
+									const uint32_t& timeout_ms	)		const;
 
-    void			give_semaphore			( void ) const;
+    BASE_RESULT		getByte		( uint8_t* data )						const;
+
+    void			irqHandler				( void )					const;
+
+    /// Для внутреннего пользования HAL-а.
+    void			giveSemaphore			( void )					const;
 
 private:
-    bool 			init_clk	 			( void ) const;
+    bool 			clkInit	 				( void )					const;
 
-    const uart_cfg*										const cfg;
+    const uartCfg*										const cfg;
     mutable DMA_HandleTypeDef							hdma_tx;
     mutable	UART_HandleTypeDef							handle;
     mutable USER_OS_STATIC_BIN_SEMAPHORE				s = nullptr;
